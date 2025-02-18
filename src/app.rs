c84071769e6156017c1bc87317e9506bf77ea356
George de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2025-02-18 13:42:56 +0100

    Parallel Ray tracing

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use std::sync::Mutex;
 use std::{sync::Arc,   time::Instant};
 
 use cgmath::{InnerSpace, Matrix4, Vector3, Vector4};
@@ -6,6 +7,7 @@ use egui_glium::egui_winit::egui::ImageData;
 use glium::{glutin::surface::WindowSurface, index::NoIndices, winit::{event::{DeviceEvent, ElementState, MouseButton}, keyboard::KeyCode, window::Window}, Display, DrawParameters, Program};
 use glium::winit::application::ApplicationHandler;
 use ::image::ImageBuffer;
+use crate::vertex::ShapeWorld;
 use crate::{matrix::ToArr, shader, vertex:: Shape};
 use glium::Surface;
 use glium::winit::event::WindowEvent;
@@ -16,8 +18,8 @@ use crate::camera::{Camera, CameraController, Projection};
 use std::f32::consts::FRAC_PI_2;
 const SAFE_FRAC_PI_2: f32 = FRAC_PI_2 - 0.0001;
 
-const RAY_HEIGHT: usize = 500;
-const RAY_WIDTH: usize = 500;
+const RAY_HEIGHT: usize = 1500;
+const RAY_WIDTH: usize = 1500;
 
 // Deal with application State
 // RN, only does 
@@ -37,6 +39,7 @@ pub struct App<'a> {
     mouse_press: bool,
     mouse_on_ui: bool,
     ray_trace_display: bool,
+    ray_trace_save: bool,
     ray_tace_file_name: String,
     raytrace_handler: TextureHandle,
 
@@ -48,7 +51,11 @@ impl App<'_> {
     pub fn new<'a>(window: Window, display: Display<WindowSurface>, ui: egui_glium::EguiGlium) -> App<'a> {
 
 
-        let shapes = vec![Shape::b(&display), Shape::a(&display), Shape::f(&display), Shape::floor(&display),  ];
+        let shapes = vec![
+            Shape::b(&display), 
+            Shape::a(&display), 
+            Shape::f(&display), 
+            Shape::floor(&display),  ];
 
         let vertex_shader = shader::load_shader("./shaders/vertex.glsl");
         let fragment_shader = shader::load_shader("./shaders/fragment.glsl");
@@ -76,7 +83,7 @@ impl App<'_> {
         };
         let last_step = Instant::now();
 
-        App{window, display, last_step,  camera, projection, controller,  shapes, indices, program, draw_params, ui,  mouse_press: false, mouse_on_ui: false, ray_trace_display: false, ray_tace_file_name: "test.png".to_string(), raytrace_handler}
+        App{window, display, last_step,  camera, projection, controller,  shapes, indices, program, draw_params, ui,  mouse_press: false, mouse_on_ui: false, ray_trace_display: false, ray_trace_save: true, ray_tace_file_name: "test.png".to_string(), raytrace_handler}
     }
     pub fn draw_debug(&mut self) {
 
@@ -84,6 +91,8 @@ impl App<'_> {
         frame.clear_color_and_depth((0.0, 0.0,1.0 , 1.0), 1.0);
 
         for shape in self.shapes.iter_mut() {
+            // lock shape:
+            let mut world = shape.world.lock().unwrap();
 
             let scale_matrix: Matrix4<f32> = Matrix4::from_cols(
                 Vector4::new(shape.ui_state.size.0, 0.0, 0.0, 0.0), 
@@ -91,8 +100,8 @@ impl App<'_> {
                 Vector4::new(0.0, 0.0, 1.0, 0.0),
                 Vector4::new(0.0, 0.0, 0.0, 1.0),
             );
-            let matrix = Matrix4::<f32>::from_translation(Vector3::new(0.0,0.0, shape.ui_state.distance))* shape.model_matrix * scale_matrix;
-            shape.placement_matrix = matrix;
+            let matrix = Matrix4::<f32>::from_translation(Vector3::new(0.0,0.0, shape.ui_state.distance))* world.model_matrix * scale_matrix;
+            world.placement_matrix = matrix;
 
 
             let view_proj = self.projection.calc_matrix() * self.camera.calc_matrix();
@@ -103,7 +112,7 @@ impl App<'_> {
                 tex: &shape.texture,
                 view_proj: view_proj.to_arr(),
             };
-            let buffer = glium::VertexBuffer::new(&self.display, &shape.vertex_buffer).unwrap();
+            let buffer = glium::VertexBuffer::new(&self.display, &world.vertex_buffer).unwrap();
 
             frame.draw(&buffer, self.indices, &self.program, &uniforms,&self.draw_params).unwrap();
 
@@ -139,9 +148,9 @@ impl App<'_> {
 
                 egui::Window::new("RAY TRACER").anchor(Align2::RIGHT_TOP, Vec2::new(1.0,1.0)).default_open(false).show(ctx, |ui| {
                     ui.add(
-                        egui::Image::new(&self.raytrace_handler)
+                        egui::Image::new(&self.raytrace_handler).max_size(Vec2::new(500.0, 500.0))
                     );
-                    ui.checkbox(&mut self.ray_trace_display, "Save as file");
+                    ui.checkbox(&mut self.ray_trace_save, "Save as file");
                     ui.text_edit_singleline(&mut self.ray_tace_file_name);
                 });
 
@@ -176,15 +185,23 @@ impl App<'_> {
         let q_y = (2.0 * g_y) / (image_width as f32 - 1.0) * v_n;
         let p_1_m = t_n * d - g_x * b_n - g_y * v_n;
 
-        let buf = ImageBuffer::from_fn(image_width, image_height, |x, y| {
+        let ray_origin = self.camera.position;
+        let shapes: Vec<Arc<Mutex<ShapeWorld>>> = self.shapes.iter().map(|x|  x.world.clone()).collect();
+
+
+
+        let buf = ImageBuffer::from_par_fn(image_width, image_height, |x, y| {
 
             let f_y = y as f32;
             let f_x = x as f32;
-            let ray_dir = (p_1_m + q_x*(f_x - 1.0) + q_y *(f_y - 1.0)).normalize();
-            let ray_origin = self.camera.position;
             let  color = [0_u8, 0_u8, 0_u8, 0_u8];
+            let ray_dir = (p_1_m + q_x*(f_x - 1.0) + q_y *(f_y - 1.0)).normalize();
+            
             // TODO: Right now the order of the shapes matters! Need to change that
-            for shape in self.shapes.iter() {
+            for shape in shapes.clone() {
+                let shape = shape.lock().unwrap();
+
+
                 let intersect = shape.intersect(ray_origin, ray_dir);
                 if let Some((color, _point)) = intersect {
                     return image::Rgba(color);
@@ -195,7 +212,7 @@ impl App<'_> {
 
             image::Rgba(color)
         });
-        if self.ray_trace_display {
+        if self.ray_trace_save {
             let res = buf.save_with_format(self.ray_tace_file_name.clone(), image::ImageFormat::Png);
             
             if res.is_err() {
