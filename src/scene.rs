77fc950cdbee0617019166790a5b297c9f61e9de
George de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2025-05-27 14:24:19 +0200

    CLEAN: Move binds out of app state and into scene
    Part of wider effots to improve code

--- src/scene.rs
+++ src/scene.rs
@@ -1,14 +1,18 @@
 use std::path::PathBuf;
+use wgpu::{util::DeviceExt, BindGroupLayout, Queue, RenderPass};
 
 use crate::{
     camera::Camera,
     file_picker::FilePicker,
+    raytracer::RayTraceInfo,
     shape::{Quad, Shape, VWPanel},
+    texture,
 };
-use cgmath::{Matrix4, Rad, SquareMatrix, Vector2, Vector3, Vector4};
-use crevice::std140::Writer;
+use cgmath::{Matrix4, Rad, SquareMatrix, Vector3, Vector4};
+use crevice::std140::{AsStd140, Std140, Writer};
 use egui::{Color32, RichText};
 use egui_winit::egui::{self, Context};
+use wgpu::{BindGroup, Buffer, Device};
 pub trait DrawUI {
     /*
     Draw UI for this element
@@ -26,6 +30,42 @@ Scene struct. Encapsulates UI and handles access to the raw quads
 pub struct Scene {
     world: Vec<Target>,
     pub panels: Vec<ScenePanel>,
+    ray_tracer: RayTraceInfo,
+    pub target_binds: TargetBinds,
+    pub panel_binds: PanelBinds,
+    pub texture_binds: TextureBinds,
+}
+
+pub struct ScenePanel {
+    yaw: Rad<f32>,
+    pitch: Rad<f32>,
+    roll: Rad<f32>,
+    placement: Matrix4<f32>,
+    scale: Matrix4<f32>,
+    pub panel: VWPanel,
+    pub texture: FilePicker,
+    lock_pixel: bool,
+}
+
+pub struct TargetBinds {
+    pub bind_group: BindGroup,
+    pub bind_layout: BindGroupLayout,
+    pub ray_tracer_buffer: Buffer,
+    pub scene_buffer: Buffer,
+}
+pub struct TextureBinds {
+    pub bind_group: BindGroup,
+    pub bind_layout: BindGroupLayout,
+    pub tex_size_buffer: Buffer,
+    pub target_texture: texture::Texture,
+}
+
+pub struct PanelBinds {
+    pub bind_group: BindGroup,
+    pub bind_layout: BindGroupLayout,
+    pub panel_buffer: Buffer,
+    pub panel_texture: texture::Texture,
+    pub panel_bool_buffer: Buffer,
 }
 
 pub struct Target {
@@ -35,6 +75,262 @@ pub struct Target {
     placement: Matrix4<f32>,
     scale: Matrix4<f32>,
     quad: Quad,
+}
+
+impl TextureBinds {
+    pub fn new(device: &wgpu::Device, queue: &Queue) -> Self {
+        let texture_bytes = include_bytes!("../resources/textures/Aircraft_code.png");
+
+        let texture = texture::Texture::from_bytes(device, queue, texture_bytes, "Damn");
+
+        let texture_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        // This should match the filterable field of the
+                        // corresponding Texture entry above.
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 2,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        // This should match the filter﻿able field of the
+                        // corresponding Texture entry above.
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                ],
+                label: Some("texture_bind_group_layout"),
+            });
+        let text_size_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Size of texture being passed!"),
+            contents: texture.dimensions.as_std140().as_bytes(),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&texture.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&texture.sampler),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: text_size_buffer.as_entire_binding(),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
+        TextureBinds {
+            bind_group: diffuse_bind_group,
+            bind_layout: texture_bind_group_layout,
+            tex_size_buffer: text_size_buffer,
+            target_texture: texture,
+        }
+    }
+}
+
+impl TargetBinds {
+    pub fn new(device: &wgpu::Device, buffer: &[u8], rt: &RayTraceInfo) -> Self {
+        let rt_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Ray trace buffer, contains info for shooting rays"),
+            contents: rt.as_std140().as_bytes(),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let scene_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Buffer for Scene, contains all objects"),
+            contents: buffer,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let scene_bind_group = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::all(),
+                    count: None,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::all(),
+                    count: None,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                },
+            ],
+            label: Some("Binding group for Scene"),
+        });
+
+        let scene_bind = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Scene Bind"),
+            layout: &scene_bind_group,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: scene_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: rt_buffer.as_entire_binding(),
+                },
+            ],
+        });
+        TargetBinds {
+            bind_group: scene_bind,
+            ray_tracer_buffer: rt_buffer,
+            scene_buffer,
+            bind_layout: scene_bind_group,
+        }
+    }
+}
+impl PanelBinds {
+    pub fn new(device: &wgpu::Device, queue: &wgpu::Queue, buffer: &[u8]) -> Self {
+        let panel_1 = image::DynamicImage::new_rgb8(1000, 1000);
+        let panel_2 = image::DynamicImage::new_rgb8(1000, 1000);
+        let texture_vec = vec![panel_1, panel_2];
+
+        let panel_textures =
+            texture::Texture::from_images(device, queue, &texture_vec, Some("2D Panel Array"));
+        let panel_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Buffer for Panels"),
+            contents: buffer,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let panel_bind_group = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::all(),
+                    count: None,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::all(),
+                    count: None,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2Array,
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 3,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    // This should match the filterable field of the
+                    // corresponding Texture entry above.
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 4,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    // This should match the filter﻿able field of the
+                    // corresponding Texture entry above.
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("Binding group for Scene"),
+        });
+
+        let panel_textures_size_buffer =
+            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Size of texture being passed!"),
+                contents: panel_textures.dimensions.as_std140().as_bytes(),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            });
+        let panel_bool_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Boolean for panel textures"),
+            contents: 0u32.as_std140().as_bytes(),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let panel_bind = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Binding For Panel group"),
+            layout: &panel_bind_group,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: panel_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: panel_bool_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: wgpu::BindingResource::TextureView(&panel_textures.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 3,
+                    resource: wgpu::BindingResource::Sampler(&panel_textures.sampler),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 4,
+                    resource: panel_textures_size_buffer.as_entire_binding(),
+                },
+            ],
+        });
+        PanelBinds {
+            bind_layout: panel_bind_group,
+            bind_group: panel_bind,
+            panel_buffer,
+            panel_bool_buffer,
+            panel_texture: panel_textures,
+        }
+    }
 }
 
 impl Target {
@@ -107,16 +403,6 @@ impl DrawUI for Target {
 
 /// Wrapper around panel that controls access to the UI, as well as their placement
 ///
-pub struct ScenePanel {
-    yaw: Rad<f32>,
-    pitch: Rad<f32>,
-    roll: Rad<f32>,
-    placement: Matrix4<f32>,
-    scale: Matrix4<f32>,
-    pub panel: VWPanel,
-    pub texture: FilePicker,
-    lock_pixel: bool,
-}
 impl ScenePanel {
     fn place_panel(&self) -> VWPanel {
         let yaw_matrix = Matrix4::from_angle_x(self.yaw);
@@ -149,6 +435,116 @@ impl ScenePanel {
             texture: FilePicker::new("./resources/panel_compute/".to_string(), default_path),
             scale,
         }
+    }
+}
+
+impl Scene {
+    /// Make a quad with coordinates, but in scene space, not clip space
+    pub fn new(ray_tracer: RayTraceInfo, camera: &Camera, device: &Device, queue: &Queue) -> Self {
+        let place_1 = Vector4::new(0.5, 1.5, 3.0, 1.0);
+
+        let place_2 = Vector4::new(0.5, 1.5, 2.0, 1.0);
+        let target_1 = Vector4::new(0.5, 1.5, 0.0, 1.0);
+        let target_2 = Vector4::new(7.5, 1.5, 0.0, 1.0);
+        let world = vec![Target::new(target_1), Target::new(target_2)];
+        let panels = vec![ScenePanel::new(place_1, 1), ScenePanel::new(place_2, 2)];
+
+        let world_buffer = Self::world_as_bytes(&world, camera);
+        let panels_buffer = Self::panels_as_bytes(&panels, camera);
+        let target_binds = TargetBinds::new(device, &world_buffer, &ray_tracer);
+        let panel_binds = PanelBinds::new(device, queue, &panels_buffer);
+        let texture_binds = TextureBinds::new(device, queue);
+
+        Scene {
+            world,
+            panels,
+            ray_tracer,
+            target_binds,
+            panel_binds,
+            texture_binds,
+        }
+    }
+
+    pub fn update_rt_info(&mut self, camera: &Camera, height: u32, width: u32) {
+        self.ray_tracer = RayTraceInfo::test(camera, height, width);
+    }
+
+    pub fn update_draw(&self, queue: &Queue, camera: &Camera, display_panel_texture: bool) {
+        queue.write_buffer(
+            &self.target_binds.ray_tracer_buffer,
+            0,
+            self.ray_tracer.as_std140().as_bytes(),
+        );
+
+        queue.write_buffer(
+            &self.target_binds.scene_buffer,
+            0,
+            &Self::world_as_bytes(&self.world, camera),
+        );
+
+        queue.write_buffer(
+            &self.panel_binds.panel_buffer,
+            0,
+            &Self::panels_as_bytes(&self.panels, camera),
+        );
+        if display_panel_texture {
+            queue.write_buffer(
+                &self.panel_binds.panel_bool_buffer,
+                0,
+                1.as_std140().as_bytes(),
+            );
+        } else {
+            queue.write_buffer(
+                &self.panel_binds.panel_bool_buffer,
+                0,
+                0.as_std140().as_bytes(),
+            );
+        }
+    }
+    pub fn render_pass(&self, render_pass: &mut RenderPass) {
+        render_pass.set_bind_group(0, Some(&self.target_binds.bind_group), &[]);
+        render_pass.set_bind_group(1, Some(&self.texture_binds.bind_group), &[]);
+
+        render_pass.set_bind_group(2, Some(&self.panel_binds.bind_group), &[]);
+    }
+
+    /// Will always place the closest quad first
+    pub fn world_as_bytes(world: &[Target], camera: &Camera) -> [u8; 256] {
+        let mut buffer = [0u8; 256];
+        let mut writer = Writer::new(&mut buffer[..]);
+        let mut shapes: Vec<Quad> = world.iter().map(|target| target.place_target()).collect();
+
+        shapes.sort_by(|x, y| {
+            let camera_origin = camera.position;
+            let x_dist = x.distance_to(camera_origin);
+            let y_dist = y.distance_to(camera_origin);
+            x_dist.total_cmp(&y_dist)
+        });
+
+        let _count = writer.write(shapes.as_slice()).unwrap();
+        buffer
+    }
+    /// Will always place the closest panel first
+    pub fn panels_as_bytes(panels: &[ScenePanel], camera: &Camera) -> [u8; 256] {
+        let mut buffer = [0u8; 256];
+        let mut writer = Writer::new(&mut buffer[..]);
+        let mut panels: Vec<VWPanel> = panels
+            .iter()
+            .map(|x| x.place_panel())
+            .map(|x| x.border_correction())
+            .collect();
+        panels.sort_by(|x, y| x.distance_compar(y, camera.position));
+        let _count = writer.write(panels.as_slice()).unwrap();
+        buffer
+    }
+    pub fn panels_pixel_count(&self) -> [u32; 4] {
+        let mut output = [0; 4];
+        for x in 0..2 {
+            let panel = self.panels[x].panel.pixel_count;
+            output[x * 2] = panel.x;
+            output[x * 2 + 1] = panel.y;
+        }
+        output
     }
 }
 
@@ -231,64 +627,5 @@ impl DrawUI for Scene {
 
             count += 1;
         }
-    }
-}
-
-impl Scene {
-    /// Make a quad with coordinates, but in scene space, not clip space
-    pub fn test() -> Self {
-        let place_1 = Vector4::new(0.5, 1.5, 3.0, 1.0);
-
-        let place_2 = Vector4::new(0.5, 1.5, 2.0, 1.0);
-        let target_1 = Vector4::new(0.5, 1.5, 0.0, 1.0);
-        let target_2 = Vector4::new(7.5, 1.5, 0.0, 1.0);
-        Scene {
-            world: vec![Target::new(target_1), Target::new(target_2)],
-
-            panels: vec![ScenePanel::new(place_1, 1), ScenePanel::new(place_2, 2)],
-        }
-    }
-    /// Will always place the closest quad first
-    pub fn world_as_bytes(&self, camera: &Camera) -> [u8; 256] {
-        let mut buffer = [0u8; 256];
-        let mut writer = Writer::new(&mut buffer[..]);
-        let mut shapes: Vec<Quad> = self
-            .world
-            .iter()
-            .map(|target| target.place_target())
-            .collect();
-
-        shapes.sort_by(|x, y| {
-            let camera_origin = camera.position;
-            let x_dist = x.distance_to(camera_origin);
-            let y_dist = y.distance_to(camera_origin);
-            x_dist.total_cmp(&y_dist)
-        });
-
-        let _count = writer.write(shapes.as_slice()).unwrap();
-        buffer
-    }
-    /// Will always place the closest panel first
-    pub fn panels_as_bytes(&self, camera: &Camera) -> [u8; 256] {
-        let mut buffer = [0u8; 256];
-        let mut writer = Writer::new(&mut buffer[..]);
-        let mut panels: Vec<VWPanel> = self
-            .panels
-            .iter()
-            .map(|x| x.place_panel())
-            .map(|x| x.border_correction())
-            .collect();
-        panels.sort_by(|x, y| x.distance_compar(y, camera.position));
-        let _count = writer.write(panels.as_slice()).unwrap();
-        buffer
-    }
-    pub fn panels_pixel_count(&self) -> [u32; 4] {
-        let mut output = [0; 4];
-        for x in 0..2 {
-            let panel = self.panels[x].panel.pixel_count;
-            output[x * 2] = panel.x;
-            output[x * 2 + 1] = panel.y;
-        }
-        output
     }
 }
